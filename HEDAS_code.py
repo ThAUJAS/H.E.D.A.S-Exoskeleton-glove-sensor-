e1b818224cc0a959d01a28e1259aa9866a14590f
Thomas Aujas <thomas.aujas086@gmail.com>
2021-07-13 02:13:41 +0200

    changes

--- HEDAS_code.py
+++ HEDAS_code.py
@@ -25,7 +25,8 @@ arduino = ""
 angles_raw = [0]*20
 angles = [0]*24
 angle_calibration = [0,45,90]
-
+angle_calibration_thumb = [0,45,80]
+Unity = False
 # Variables for calibration
 x_MCP = [[0] * 3 for i1 in range(5)]
 x_PIP = [[0] * 3 for i1 in range(5)]
@@ -54,19 +55,16 @@ def Calibration(portDevice):
     global master
     global ready
     global arduino
-    arduino = serial.Serial(port=portDevice, baudrate=9600, timeout=.1)
+    arduino = serial.Serial(port=portDevice, baudrate=115200, timeout=.1)
     ready = True
     eraseWidget()
-    try:
-        with open("c:/TRAVAIL/MSc project/code python/sample.json") as json_open:
-            data = json.load(json_open)
+    if os.path.isfile(os.path.join(path + os.sep, "sample.json")):
         Label(master, text ="Do you want to calibrate or use a previous calibration profile?", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack(pady = 150)
         Button(master, text = "New calibration", command = Cal, font=("Abadi MT Condensed Extra Bold", 30), bg = 'snow').pack(pady = 10)
         Button(master, text = "Use a user calibration profile", command = NoCal, font=("Abadi MT Condensed Extra Bold", 30), bg = 'snow').pack(pady = 50)
-    except:
-        Label(master, text ="You didn't calibrate yet... Create a new calibration", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
+    else:
+        Label(master, text ="You didn't calibrate yet... Create a new calibration", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack(pady = 150)
         Button(master, text = "New calibration", command = Cal, font=("Abadi MT Condensed Extra Bold", 30), bg = 'snow').pack(pady = 10)
-
 
 def Cal():
     global master
@@ -74,7 +72,7 @@ def Cal():
     eraseWidget()
 
     if val == 0:
-        Label(master, text ="Put your finger at 0° and close to each other", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
+        Label(master, text ="Put your hand really flat on a surface with the thumb as far as possible", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
         image1 = Image.open(os.path.join(path + os.sep, "0 above.jpg"))
         image1 = image1.resize((800, 600), Image.ANTIALIAS)
         test = ImageTk.PhotoImage(image1)
@@ -82,7 +80,7 @@ def Cal():
         label1.image = test
         label1.pack(side = TOP, pady = 25)
     if val == 1:
-        Label(master, text ="Put your finger at 45°", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
+        Label(master, text ="Close your hand so that every joint form a 45° angle", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
         image1 = Image.open(os.path.join(path + os.sep, "45 profile.jpg"))
         image1 = image1.resize((720, 540), Image.ANTIALIAS)
         test1 = ImageTk.PhotoImage(image1)
@@ -96,7 +94,19 @@ def Cal():
         label1.place(x=400,y = master.winfo_height()/2,anchor = CENTER)
         label2.place(x=1150,y = master.winfo_height()/2,anchor = CENTER)
     if val == 2:
-        Label(master, text ="Put your finger at 90°", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
+        Label(master, text ="Close your hand so that every joint form a 90° angle", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
+        image1 = Image.open(os.path.join(path + os.sep, "90 profile.jpg"))
+        image1 = image1.resize((720, 540), Image.ANTIALIAS)
+        test1 = ImageTk.PhotoImage(image1)
+        label1 = Label(image=test1, bg = 'medium aquamarine')
+        label1.image = test1
+        image2 = Image.open(os.path.join(path + os.sep, "90 bellow.jpg"))
+        image2 = image2.resize((720, 540), Image.ANTIALIAS)
+        test2 = ImageTk.PhotoImage(image2)
+        label2 = Label(image=test2, bg = 'medium aquamarine')
+        label2.image = test2
+        label1.place(x=400,y = master.winfo_height()/2,anchor = CENTER)
+        label2.place(x=1150,y = master.winfo_height()/2,anchor = CENTER)
     if val == 3:
         Label(master, text="Enter your Username", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
         entry1 = Entry(master, font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine')
@@ -110,63 +120,64 @@ def get_calibration(num):
     eraseWidget()
     if num==0:
         for i in range(0,7):
-            z_MCP[i] = angles_raw[i+11]
+            z_MCP[i] = angles_raw[i+10]
     for j in range(0,5):
-        x_MCP[j][num] = angles_raw[j]
-        x_PIP[j][num] = angles_raw[j+5]
+        x_PIP[j][num] = angles_raw[j]
+        x_MCP[j][num] = angles_raw[j+5]
     val = val + 1
     if val == 3:
         for t in range(0,5):
             coeff_MCP[t] = np.polyfit(x_MCP[t],angle_calibration,2).tolist()
             coeff_PIP[t] = np.polyfit(x_PIP[t],angle_calibration,2).tolist()
+        print(coeff_MCP)
+        coeff_MCP[0] = np.polyfit(x_MCP[0],angle_calibration_thumb,2).tolist()
+
     Cal()
 
 def getName(name):
-    try:
-        with open(path, "sample.json") as json_open:
+    if os.path.isfile(os.path.join(path + os.sep, "sample.json")):
+        with open(os.path.join(path + os.sep, "sample.json")) as json_open:
             data = json.load(json_open)
+            
+        data['Usernames'].append({'name': name,'PIP': coeff_PIP,'MPCx': coeff_MCP,'MPCz': z_MCP})
 
-        data['Usernames'].append({'name': name,'PIP': coeff_MCP,'MPCx': coeff_MCP,'MPCz': z_MCP})
-
-        with open("c:/TRAVAIL/MSc project/code python/sample.json","w") as json_write:
+        with open(os.path.join(path + os.sep, "sample.json"),"w") as json_write:
             json.dump(data,json_write)
-    except:
+    else:
         data = {}
         data['Usernames'] = []
-        data['Usernames'].append({'name': name,'PIP': coeff_MCP,'MPCx': coeff_MCP,'MPCz': z_MCP})
+        data['Usernames'].append({'name': name,'PIP': coeff_PIP,'MPCx': coeff_MCP,'MPCz': z_MCP})
 
-        with open("c:/TRAVAIL/MSc project/code python/sample.json", "w") as json_create:
+        with open(os.path.join(path + os.sep, "sample.json"), "w") as json_create:
             json.dump(data, json_create)
     final_page()
 
 def NoCal():
     eraseWidget()
     Label(master, text ="Choose your calibration profile", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack(pady = 100)
-    with open("c:/TRAVAIL/MSc project/code python/sample.json") as json_open:
+    with open(os.path.join(path + os.sep, "sample.json")) as json_open:
         data = json.load(json_open)
-    for user in data['Usernames'] :  # button of the name of the user
-        Button(master, text = user['name'], command = lambda : get_coeff(user['PIP'],user['MPCx'],user['MPCz']), font=("Abadi MT Condensed Extra Bold", 30), bg = 'snow').pack(pady = 10)
+    for user in data['Usernames'] : 
+        Button(master, text = user['name'], command = lambda username = user: get_coeff(username['PIP'],username['MPCx'],username['MPCz']), font=("Abadi MT Condensed Extra Bold", 30), bg = 'snow').pack(pady = 10)
 
 # function that replace the coefficient from the JSON
 def get_coeff(PIP, MPCx, MPCz):
     global coeff_MCP, coeff_PIP, z_MCP
-    coeff_MCP = MPCx
     coeff_PIP = PIP
+    coeff_MCP = MPCx
     z_MCP = MPCz
     final_page()
 
 def final_page():
-    eraseWidget()
-    Label(master, text ="You are ready to use the glove", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
-    Label(master, text ="The angles are avaibles in the array 'angles'", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
-
-def Unity():
-    eraseWidget()
-    '''host, port = "127.0.0.1", 25001
+    global Unity,sock
+    Unity = True
+    host, port = "127.0.0.1", 25001
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))'''
-    print('Connected to UNITY')
-
+    sock.connect((host, port))
+    eraseWidget()
+    Label(master, text ="You are ready to use the glove", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack(pady = 150)
+    Label(master, text ="The angles are avaibles in the array 'angles'", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack(pady = 10)
+    Label(master, text ="Or try in UNITY", font=("Abadi MT Condensed Extra Bold", 30), bg = 'medium aquamarine').pack()
 
 #---------------function/thread to read the data from the serial port----------------#
 class thread_with_trace(Thread):
@@ -201,29 +212,34 @@ class thread_with_trace(Thread):
   
 def func():
   while True:
-    global ready
-    global angles_raw
-    global angles
+    global ready,angles_raw,angles,Unity,sock
     if ready == True:
         data = arduino.readline()[:-2]
         if data:
             angles_raw = [float(x) for x in data.split()] 
-            print(angles_raw)
+            
             for i in range (0,5):
                 # PIP
                 angles[i] = poly_reg(coeff_PIP[i], angles_raw[i])
                 # MCPx
-                angles[i+5]= poly_reg(coeff_MCP[i],angles_raw[i+5])
+                angles[i+5]= poly_reg(coeff_MCP[i], angles_raw[i+5])
+            
             for i in range (0,7):
                 # MCPz
-                angles[i+10] = (potToAngle(angles_raw[i+10])-z_MCP[i])
+                angles[i+10] = potToAngle(z_MCP[i])-potToAngle(angles_raw[i+10])
+            
             for i in range(0,3):
                 #IMU angles
                 angles[i+17] = angles_raw[i+17]
+                
             for i in range (0,4): 
                 # DIP
                 angles[i+20] = angles[i+1]*0.88
-            print(angles)
+                
+            for i in range (0,24):
+                angles[i] = int(angles[i]*1000)
+            if Unity:
+                sock.sendall(json.dumps(angles).encode())
 
 def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
--- general_functions.py
+++ general_functions.py
@@ -5,9 +5,3 @@ def poly_reg(coeff,val):
 def potToAngle(val):
     return val * 330 / 1023 + 345
 
-def median_filter(xoldvalMedian,raw,size_filter):
-    if len(xoldvalMedian)==size_filter:
-        xoldvalMedian.pop(0)
-    xoldvalMedian.append(raw)
-    filteredMedian = np.median(xoldvalMedian)
-    return filteredMedian
